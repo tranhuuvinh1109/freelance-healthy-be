1e436010f6fc7b1dc77495a8477b22f710739637
VinhHuuTran <tranhuuvinh1109@gmail.com>
2024-09-05 08:56:36 +0700

    update api testing and update method realtime

--- src/routes/RealtimeRoute.js
+++ src/routes/RealtimeRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const RealtimeController = require("../app/controllers/RealtimeController");
 const router = express.Router();
 
-router.get("/write", RealtimeController.write);
-router.get("/read", RealtimeController.read);
+router.post("/write", RealtimeController.write);
+router.post("/read", RealtimeController.read);
 
 module.exports = router;
